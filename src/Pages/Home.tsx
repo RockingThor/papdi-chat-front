512b0fbd66952094138793db965503a1d006114d
Rohit Nandi <rohitnandibmp@gmail.com>
2024-09-14 19:09:46 +0530

    some mods

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,7 +5,7 @@ import Signup from "@/components/Signup";
 const Home = () => {
   return (
     <div className="home__container">
-      <Tabs defaultValue="login" className="w-[400px]">
+      <Tabs defaultValue="login" className="w-[400px] mt-40">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="signup">Signup</TabsTrigger>
